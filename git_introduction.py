f22ec9c53bbec843d68a8b3527f076014d630446
Wayan darma <106679234+darma2703@users.noreply.github.com>
2023-09-26 02:38:29 -1100

    initial commit

---
+++ git_introduction.py
@@ -0,0 +1,30 @@
+print("hello world")
+
+# make a simple calculator
+# add two numbers
+# subtract two numbers
+# multiply two numbers
+# divide two numbers
+
+num1 = 10
+num2 = 5
+
+# add two numbers
+print(num1 + num2)
+
+# subtract two numbers
+print(num1 - num2)
+
+# multiply two numbers
+print(num1 * num2)
+
+# divide two numbers
+print(num1 / num2)
+
+# modulo two numbers
+print(num1 % num2)
+
+if num1 > num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
